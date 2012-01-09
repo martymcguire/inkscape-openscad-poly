54deec9e33ba7a3004a01b77a76c644ee1acfabd
Marty McGuire <robert.m.mcguire@gmail.com>
2012-01-09 10:30:28 -0500

    Clarify what `pfloat`s are and what they do.

--- src/openscad_poly/context.py
+++ src/openscad_poly/context.py
@@ -1,7 +1,8 @@
 from math import *
 import sys
 
-class pfloat(float):
+class shorter_float(float):
+    """A float which returns only 3 digits after the decimal"""
     def __repr__(self):
       return "%0.3f" % self
 
@@ -21,5 +22,5 @@ class OSCADPolyContext:
         print "    );}"
 
     def add_poly(self, id, points, paths):
-      newpoints = [[pfloat(x), pfloat(y)] for x, y in points]
-      self.polygons.append({ 'id': id, 'points':newpoints, 'paths':paths})
+      shortened_points = [[shorter_float(x), shorter_float(y)] for x, y in points]
+      self.polygons.append({ 'id': id, 'points':shortened_points, 'paths':paths})
